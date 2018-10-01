20654551f9a8cc4475be05536a754d4175ea5868
BrianHo <Curry30@Brians-MacBook-Pro.local>
2018-10-01 16:17:04 -0700

    push

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Type_Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // // Implicit Casting
+            // int IntegerValue = 65;
+            // double DoubleValue = IntegerValue;
+
+            // //Explicit Casting
+            // double DoubleValue = 3.14159265358;
+            // int IntegerValue = (int)DoubleValue;
+
+            // doble floatNum = 7.76;
+            // int num = floatNum;
+            // //This will cause a compiler type-mismatch error!!
+            // int num = (int)floatNum;
+            // //Floating point to integer conversion truncates all numbers after the decimal
+            // //As such num now evaluated to 7
+
+            // int num = (int)"24";
+            // // This, however, will not work as strings can not be type cast to int
+            // //Converting an integer to a string
+            // int num = 7;
+            // string numAsString = num.ToString(); //Evaluates as "7"
+            // //Converting that string value to a double
+            // doublecopy stringAsDouble = numAsString.ToDouble(); //Evaluates as 7.0
+
+            
+        }
+    }
+}
